8a54cb3fce7580282ba4c9a57d25c5bfa8d40c92
Pedro Henauth <pedrohenauth1212@gmail.com>
2025-02-25 20:14:46 -0300

    ASP-0002 - Ajuste de anotação

--- src/main/java/com/aws/spring/project/rest/representation/UserRepresentation.java
+++ src/main/java/com/aws/spring/project/rest/representation/UserRepresentation.java
@@ -1,11 +1,11 @@
 package com.aws.spring.project.rest.representation;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 @Data
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserRepresentation {
 
     private String name;
